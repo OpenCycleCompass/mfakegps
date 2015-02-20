c4697748706ee4eaa42940b6229e5d960d248332
Raphael Lehmann <raphael@rleh.de>
2015-02-20 23:46:14 +0100

    button functions & logging removed menu

--- app/src/main/java/de/mytfg/jufo/mfakegps/MainActivity.java
+++ app/src/main/java/de/mytfg/jufo/mfakegps/MainActivity.java
@@ -3,6 +3,7 @@ package de.mytfg.jufo.mfakegps;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+    // Log TAG
+    protected static final String TAG = "MainActivity-class";
 
     private Button button_rec;
     private Button button_play;
@@ -28,16 +31,32 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     public void play(View view){
-
+        Log.i(TAG, "play()");
+        if(rec) {
+            // Warn user by toast
+            return;
+        }
+        Intent intent_play = new Intent(this, FakeGPSRec.class);
+        if(play) {
+            stopService(intent_play);
+            button_play.setText(R.string.button_play_track);
+            button_rec.setEnabled(true);
+            play = false;
+        }
+        else {
+            startService(intent_play);
+            button_play.setText(R.string.button_play_track_stop);
+            button_rec.setEnabled(false);
+            play = true;
+        }
     }
 
     public void rec(View view){
+        Log.i(TAG, "rec()");
         if(play) {
             // Warn user by toast
             return;
@@ -45,24 +64,20 @@ public class MainActivity extends ActionBarActivity {
         Intent intent_rec = new Intent(this, FakeGPSRec.class);
         if(rec) {
             stopService(intent_rec);
+            button_rec.setText(R.string.button_record_track);
+            button_play.setEnabled(true);
+            rec = false;
         }
         else {
             startService(intent_rec);
+            button_rec.setText(R.string.button_record_track_stop);
+            button_play.setEnabled(false);
+            rec = true;
         }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
