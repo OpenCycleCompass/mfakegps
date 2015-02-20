f747e8f9772a5772e5cfe87403a61e7d147a7bad
Raphael Lehmann <raphael@rleh.de>
2015-02-20 23:28:43 +0100

    added buttons and basic functionality to MFakeGPS app

--- app/src/main/java/de/mytfg/jufo/mfakegps/MainActivity.java
+++ app/src/main/java/de/mytfg/jufo/mfakegps/MainActivity.java
@@ -1,17 +1,28 @@
 package de.mytfg.jufo.mfakegps;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button button_rec;
+    private Button button_play;
+
+    private boolean rec = false;
+    private boolean play = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_rec = (Button) findViewById(R.id.button_record);
+        button_play = (Button) findViewById(R.id.button_play);
     }
 
 
@@ -20,6 +31,24 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void play(View view){
+
+    }
+
+    public void rec(View view){
+        if(play) {
+            // Warn user by toast
+            return;
+        }
+        Intent intent_rec = new Intent(this, FakeGPSRec.class);
+        if(rec) {
+            stopService(intent_rec);
+        }
+        else {
+            startService(intent_rec);
+        }
     }
 
     @Override
